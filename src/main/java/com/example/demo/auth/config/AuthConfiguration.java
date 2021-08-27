1aa7d949d4b15cb9d19af28d0edd8436f4b724c2
Melnicov <melnicov.aleksandr@gmail.com>
2021-08-27 17:53:01 +0300

    - added secured endpoint with cognito token verification

---
+++ src/main/java/com/example/demo/auth/config/AuthConfiguration.java
@@ -0,0 +1,37 @@
+package com.example.demo.auth.config;
+
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.nimbusds.jose.JWSAlgorithm.RS256;
+
+@Configuration
+public class AuthConfiguration {
+    @Autowired
+    private JwtConfiguration jwtConfiguration;
+
+    @Bean
+    public ConfigurableJWTProcessor configurableJWTProcessor() throws MalformedURLException {
+        ResourceRetriever resourceRetriever =
+                new DefaultResourceRetriever(jwtConfiguration.getConnectionTimeout(),
+                        jwtConfiguration.getReadTimeout());
+        URL jwkSetURL= new URL(jwtConfiguration.getJwkUrl());
+        JWKSource keySource= new RemoteJWKSet(jwkSetURL, resourceRetriever);
+        ConfigurableJWTProcessor jwtProcessor= new DefaultJWTProcessor();
+        JWSKeySelector keySelector= new JWSVerificationKeySelector(RS256, keySource);
+        jwtProcessor.setJWSKeySelector(keySelector);
+        return jwtProcessor;
+    }
+}
